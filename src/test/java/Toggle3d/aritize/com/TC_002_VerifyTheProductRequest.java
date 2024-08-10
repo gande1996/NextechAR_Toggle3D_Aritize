5d1ca04719171f55d8f456a0cdea5a7fc28bd319
akhil <akhilkumargqandhe@gmail.com>
2024-08-11 00:50:21 +0530

    productrequest

--- src/test/java/Toggle3d/aritize/com/TC_002_VerifyTheProductRequest.java
+++ src/test/java/Toggle3d/aritize/com/TC_002_VerifyTheProductRequest.java
@@ -1,12 +1,9 @@
 package Toggle3d.aritize.com;
 
-
-
-
-
 import static org.testng.Assert.assertEquals;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,7 +11,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -44,7 +43,8 @@ public class TC_002_VerifyTheProductRequest {
 	@Test
 	public void verify_with_valid_login_details() {
 		driver.findElement(By.linkText("Products")).click();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.navigate().refresh();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
 		driver.findElement(By.xpath("(//span[normalize-space()='Product Request'])[1]")).click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 //		String text = driver.findElement(By.cssSelector("h2")).getText();
@@ -53,13 +53,38 @@ public class TC_002_VerifyTheProductRequest {
 		productTitle.sendKeys("pumaShoes");
 		WebElement productURL = driver.findElement(By.id("url"));
 		productURL.sendKeys("https://in.puma.com/in/en/pd/tour-mens-sneakers/386918?swatch=01");
-		WebElement sku=driver.findElement(By.id("sku"));
+		WebElement sku = driver.findElement(By.id("sku"));
 		sku.sendKeys("puma001");
 		WebElement productcategory = driver.findElement(By.id("client_category"));
 		productcategory.click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		Select productcategory2 = new Select(driver.findElement(By.id("client_category")));
-		productcategory2.selectByVisibleText("Cricket Products");
+		WebElement categoryInput = driver.findElement(By.id("client_category"));
+		categoryInput.sendKeys("Cricket Products");
+		List<WebElement> dropdownOptions = driver
+				.findElements(By.xpath("//li[contains(@class, 'MuiAutocomplete-option')]"));
+
+		for (WebElement option : dropdownOptions) {
+			if (option.getText().equals("Cricket Products")) {
+				option.click();
+				break;
+			}
+		}
+		WebElement height = driver.findElement(By.id("heightValue"));
+		height.sendKeys("8");
+		WebElement width = driver.findElement(By.id("widthValue"));
+		width.sendKeys("5");
+		WebElement depth = driver.findElement(By.id("depthValue"));
+		depth.sendKeys("4");
+		WebElement note=driver.findElement(By.id("note"));
+		note.sendKeys("newmodel");
+		driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/div[3]/div/div/div[8]/div/div[1]/div/div/div/button[2]/span[1]")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		driver.findElement(By.id("url-0")).sendKeys("https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/107341/03/sv01/fnd/IND/fmt/png/Varion-II-Unisex-Indoor-Sports-Shoes");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		WebElement sendRequest = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/div[4]/button[2]"));
+		boolean flag=sendRequest.isEnabled();
+		sendRequest.click();
+		
 		
 		
 	}
@@ -67,6 +92,6 @@ public class TC_002_VerifyTheProductRequest {
 	@AfterMethod
 	public void closures() {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//		driver.quit();Test2
+//		driver.quit();
 	}
 }
