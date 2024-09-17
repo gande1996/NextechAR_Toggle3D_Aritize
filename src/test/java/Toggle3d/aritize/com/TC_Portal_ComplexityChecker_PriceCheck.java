b878cdd41ac51ac2286d139a9ccff48b8581bf83
akhil <akhilkumargqandhe@gmail.com>
2024-09-17 23:29:38 +0530

    17sep2024-complexitychecker

--- src/test/java/Toggle3d/aritize/com/TC_Portal_ComplexityChecker_PriceCheck.java
+++ src/test/java/Toggle3d/aritize/com/TC_Portal_ComplexityChecker_PriceCheck.java
@@ -1,7 +1,5 @@
 package Toggle3d.aritize.com;
 
-import static org.testng.Assert.assertEquals;
-
 import java.time.Duration;
 import java.util.List;
 
@@ -10,17 +8,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class testcase1 {
+public class TC_Portal_ComplexityChecker_PriceCheck {
+
 	WebDriver driver;
 
 	@BeforeMethod
@@ -100,5 +96,4 @@ public class testcase1 {
 	public void close() {
 //		driver.close();
 	}
-
 }
